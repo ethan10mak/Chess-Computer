02f7e97def2e251bd71568c88b84fa5ce0bc6a22
ethan10mak <ethan10mak@gmail.com>
2023-02-23 00:46:10 -0800

    Update Board.py

--- Board.py
+++ Board.py
@@ -4,17 +4,26 @@ from tkinter import *
 
 def drawBoard(x, y):
     color = "white"
-    while x <= 400:
-        while y <= 400:
+    while y < 400:
+        while x < 400:
             canvas.create_rectangle(x, y, x+50, y+50,
              outline="black", fill=color)
             if color == "white":
+                #print("w")
                 color = "black"
             else:
+                #print("b")
                 color = "white"
-            y = y + 50
-        y = 0
-        x = x + 50
+            print(x, y)
+            x = x + 50
+        x = 0
+        y = y + 50
+        if color == "white":
+            #print("w")
+            color = "black"
+        else:
+            #print("b")
+            color = "white"
     
 
 
@@ -23,10 +32,10 @@ board = chess.Board()
 print(board)
 
 window = Tk()
-canvas = Canvas(height = 800, width = 600)
+
 window.title("Chess")
 window.geometry("800x600")
-
+canvas = Canvas(height = 800, width = 600)
 drawBoard(0,0)
 canvas.pack()
 
