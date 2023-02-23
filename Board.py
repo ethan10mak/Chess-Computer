130f5e518c001fa48ff8cb37320a40836f4fc222
ethan10mak <ethan10mak@gmail.com>
2023-02-23 00:30:56 -0800

    Update Board.py
    Made the background board

--- Board.py
+++ Board.py
@@ -1,14 +1,35 @@
 import numpy as np
 import chess
-import tkinter as tk
+from tkinter import *
+
+def drawBoard(x, y):
+    color = "white"
+    while x <= 400:
+        while y <= 400:
+            canvas.create_rectangle(x, y, x+50, y+50,
+             outline="black", fill=color)
+            if color == "white":
+                color = "black"
+            else:
+                color = "white"
+            y = y + 50
+        y = 0
+        x = x + 50
+    
 
 
 
 board = chess.Board()
 print(board)
 
-window = tk.Tk()
-
+window = Tk()
+canvas = Canvas(height = 800, width = 600)
 window.title("Chess")
-window.geometry("600x600")
+window.geometry("800x600")
+
+drawBoard(0,0)
+canvas.pack()
+
+
 window.mainloop()
+
